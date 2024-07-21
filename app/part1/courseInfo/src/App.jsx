a15f22ba5784459d0150b8242ce88260f21bbc89
Jose González <jose.gonzalez@idealista.com>
2024-07-21 13:24:09 +0200

    add the total component

--- app/part1/courseInfo/src/App.jsx
+++ app/part1/courseInfo/src/App.jsx
@@ -12,6 +12,12 @@ const Content = (props) => {
     )
 }
 
+const  Total = (props) => {
+    return (
+        <p>Number of exercises <b>{props.total}</b></p>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -24,10 +30,12 @@ const App = () => {
     return (
         <div>
             <Header course={course}/>
+
             <Content part={part1} exercises={exercises1}/>
             <Content part={part2} exercises={exercises2}/>
             <Content part={part3} exercises={exercises3}/>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+            <Total total={exercises1 + exercises2 + exercises3}/>
         </div>
     )
 }
