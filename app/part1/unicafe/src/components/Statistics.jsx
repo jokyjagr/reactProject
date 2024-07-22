70d644df65213c277f240ed80b23c8e0826a7685
Jose González <jose.gonzalez@idealista.com>
2024-07-22 19:03:46 +0200

    1.9: unicafe paso 4

--- app/part1/unicafe/src/components/Statistics.jsx
+++ app/part1/unicafe/src/components/Statistics.jsx
@@ -2,17 +2,27 @@ import StatisticLine from "./StatisticLine.jsx";
 
 const Statistics = (props) => {
     let statistics = props.values;
-    return (
-        <>
-            <StatisticLine text="Good: " value={statistics.good}/>
-            <StatisticLine text="Neutral: " value={statistics.neutral}/>
-            <StatisticLine text="Bad: " value={statistics.bad}/>
-            <StatisticLine text="All: " value={statistics.all}/>
-            <StatisticLine text="Average: " value={statistics.average}/>
-            <StatisticLine text="Positive: " value={statistics.positive}/>
-        </>
 
-    )
+    if (statistics.all === 0) {
+        return (
+            <>
+                {"No Feedback given"}
+            </>
+        )
+    } else {
+        return (
+            <>
+                <StatisticLine text="Good: " value={statistics.good}/>
+                <StatisticLine text="Neutral: " value={statistics.neutral}/>
+                <StatisticLine text="Bad: " value={statistics.bad}/>
+                <StatisticLine text="All: " value={statistics.all}/>
+                <StatisticLine text="Average: " value={statistics.average}/>
+                <StatisticLine text="Positive: " value={statistics.positive}/>
+            </>
+        )
+    }
+
+
 }
 
 export default Statistics;
