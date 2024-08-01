0cf2ac73092b272d3ad97844e9248353cfe4d155
Jose González <jose.gonzalez@idealista.com>
2024-08-01 16:47:25 +0200

    2.13: La Agenda Telefónica paso 8

--- app/app/src/App.jsx
+++ app/app/src/App.jsx
@@ -123,7 +123,7 @@ const App = () => {
     const toggleImportanceOf = id => {
         const url = `http://localhost:3001/notes/${id}`
         const note = notes.find(n => n.id === id)
-        const changedNote = { ...note, important: !note.important }
+        const changedNote = {...note, important: !note.important}
 
         noteService
             .update(id, changedNote)
